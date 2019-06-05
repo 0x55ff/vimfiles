d9ef72e572358a9ae7c8d73ea7ae73cc09b42798
0x55ff <0x55ff0x@gmail.com>
2019-06-05 15:02:34 +0800

    add swapfiles directory

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 script_dir=$(dirname $(realpath $0))
 
-
 if [ ! -f $script_dir/autoload/plug.vim ];then
     curl -fLo $script_dir/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,10 +11,11 @@ if [ ! -d $script_dir/plugged ];then
     mkdir -p $script_dir/plugged
 fi
 
-if [ ! -d $script_dir/plugged/vim-quantum ];then
-    cd $script_dir/plugged
-    git clone https://github.com/tyrannicaltoucan/vim-quantum.git
-fi
+
+#if [ ! -d $script_dir/plugged/vim-quantum ];then
+#    cd $script_dir/plugged
+#    git clone https://github.com/tyrannicaltoucan/vim-quantum.git
+#fi
 
 if ! [  $script_dir  -ef ~/.vim ];then
     if [ -e ~/.vim ];then
