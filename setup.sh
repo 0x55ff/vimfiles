f456acbca7fa776bc09280c4c5241026c6a4a0a3
0x55ff <0x55ff0x@gmail.com>
2019-06-05 10:08:16 +0800

    check repo path is ~/.vim or not

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+script_dir=$(dirname $0)
 
-git pull
-
-if [ -e "~/.vim" ];then
-    mv ~/.vim ~/.vim.bkp
+if ! [  $script_dir  -ef ~/.vim ];then
+    if [ -e ~/.vim ];then
+        mv -f ~/.vim ~/.vim.bkp
+    fi
+    cp -rf $script_dir ~/.vim
 fi
-cp -rf . ~/.vim
+
 
 if [ -f "~/.vimrc" ];then
     mv ~/.vimrc ~/.vimrc.bkp
